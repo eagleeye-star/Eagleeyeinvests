0b0340f1dc9d5152059bc06e3dedb9236a37ad90
eagleeye-star <eagleeyefx1@gmail.com>
2026-07-16 22:25:57 +0000

    Delete EagleEyE-v15.9-Vercel directory

--- EagleEyE-v15.9-Vercel/api/alerts.js
+++
@@ -1,7 +0,0 @@
-const { toEvent, send } = require('../lib/adapter');
-const handler = require('../lib/alerts');
-module.exports = async function(req, res) {
-  const event = await toEvent(req);
-  const result = await handler(event);
-  send(res, result);
-};
